484620d096b52428661ff6bb16c11fdbc084279d
Dongsik <dongsiku+git@gmail.com>
2019-11-12 09:47:19 +0900

    Update PrevNextButton.java

--- InitialScreen.java
+++ InitialScreen.java
@@ -5,7 +5,6 @@ public class InitialScreen extends JFrame {
 
     Container initialscreenContainer;
     private JLabel startLabel;
-    private JButton startButton;
 
     InitialScreen(Container initial_screen_contanier) {
         initialscreenContainer = initial_screen_contanier;
@@ -13,10 +12,14 @@ public class InitialScreen extends JFrame {
 
     public void showScreen() {
         PrevNextButton prevnextButton = new PrevNextButton(initialscreenContainer);
+
         startLabel = new JLabel("電子化手順書");
         // initialscreenContainer.add(startLabel);
         startLabel.setFont(new Font("Arial", Font.BOLD, 32));
         startLabel.setSize(startLabel.getPreferredSize());
+        startLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        initialscreenContainer.add(startLabel);
         prevnextButton.showPrevNextButton();
     }
 
--- PrevNextButton.java
+++ PrevNextButton.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class PrevNextButton extends JFrame {
 
@@ -12,12 +13,6 @@ public class PrevNextButton extends JFrame {
     }
 
     public void showPrevNextButton() {
-        Dimension prevnextButtonDimension = new Dimension(20, 20);
-        prevButton = new JButton("Previous");
-        nextButton = new JButton("Next");
-        prevButton.setPreferredSize(prevnextButtonDimension);
-        nextButton.setPreferredSize(prevnextButtonDimension);
-
         prevnextPanel = new JPanel();
         // public void setBounds(int x, int y, int width, int height);
         // int button_x = 300;
@@ -25,13 +20,28 @@ public class PrevNextButton extends JFrame {
         // prevButton.setBounds(button_x, button_y, 100, 50);
         // nextButton.setBounds(button_x, button_y, 100, 50);
         // prevnextPanel.setBounds(620, 600, 200, 50);
-        prevnextPanel.setLayout(new GridLayout(1, 2, 100, 20));
+        prevnextPanel.setLayout(new GridLayout(1, 2, 20, 20));
+        prevButton = new JButton("Previous");
+        nextButton = new JButton("Next");
+
+        Dimension prevnextButtonDimension = new Dimension(320, 80);
+        prevButton.setPreferredSize(prevnextButtonDimension);
+        nextButton.setPreferredSize(prevnextButtonDimension);
+
         prevnextPanel.add(prevButton);
         prevnextPanel.add(nextButton);
         prevnextPanel.setBackground(Color.ORANGE);
 
-        screenContainer.add(prevnextPanel, BorderLayout.SOUTH);
+        screenContainer.add(prevnextPanel, BorderLayout.NORTH);
 
+    }
+
+    public void listenPrevNextButton(JPanel cardPanel) {
+        prevButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                ((CardLayout) cardPanel.getLayout()).next(cardPanel);
+            }
+        });
     }
 
 }
