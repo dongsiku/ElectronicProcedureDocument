f264aebe2cdda1ce73438ff9841096392722c4c7
Dongsik <dongsiku+git@gmail.com>
2019-11-12 19:17:44 +0900

    Update InputIdDate.java

--- InputIdDate.java
+++ InputIdDate.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Calendar;
 
 public class InputIdDate extends JFrame {
 
     Container initialscreenContainer;
+    JLabel idLabel = new JLabel("ID: ");;
     String id_num_str = "";
 
     InputIdDate(Container initial_screen_contanier) {
@@ -13,62 +15,99 @@ public class InputIdDate extends JFrame {
 
     public void showScreen() {
         PrevNextButton prevnextButton = new PrevNextButton(initialscreenContainer);
-        JPanel panel = new JPanel(new GridLayout(3, 1));
-        JPanel id_date_panels[] = { new JPanel(new GridLayout(1, 2)), new JPanel(new GridLayout(1, 2)) };
-        JPanel software_keyboard = new JPanel(new GridLayout(4, 3));
-        JButton software_buttons[] = new JButton[10];
+        JPanel mainPanel = new JPanel(new GridLayout(3, 1));
 
-        for (int i = 0; i < 10; i++) {
-            software_buttons[i] = new JButton(String.valueOf(i));
-        }
+        mainPanel.add(createDatePanel());
+        mainPanel.add(createIdPanel());
+        mainPanel.add(createKeyboardPanel());
 
-        JLabel idLabel = new JLabel("ID: " + id_num_str);
-        id_date_panels[0].add(idLabel);
-        id_date_panels[1].add(new JLabel("DATE: "));
-        for (int i = 1; i < 10; i++) {
-            software_keyboard.add(software_buttons[i]);
-        }
-        software_keyboard.add(new JLabel(""));
-        software_keyboard.add(software_buttons[0]);
+        initialscreenContainer.add(mainPanel);
+        prevnextButton.showPrevNextButton();
+
+    }
+
+    private JPanel createIdPanel() {
+        JPanel idPanel = new JPanel(new FlowLayout());
+        idPanel.add(idLabel);
+        return idPanel;
+    }
+
+    private JPanel createKeyboardPanel() {
+        JPanel keyboardPanel = new JPanel(new GridLayout(4, 3));
+        JButton numberButtons[] = new JButton[10];
         JButton deleteButton = new JButton("Delete");
-        software_keyboard.add(deleteButton);
-        for (
-
-        JPanel id_date_panel : id_date_panels) {
-            panel.add(id_date_panel);
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
         }
 
-        panel.add(software_keyboard);
+        for (int i = 1; i < 10; i++) {
+            keyboardPanel.add(numberButtons[i]);
+        }
+        keyboardPanel.add(new JLabel(""));
+        keyboardPanel.add(numberButtons[0]);
+        keyboardPanel.add(deleteButton);
 
         for (int i = 0; i < 10; i++) {
-            String temp_id_num = String.valueOf(i);
-            software_buttons[i].addActionListener(new ActionListener() {
+            String tempNum = String.valueOf(i);
+            numberButtons[i].addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     if (id_num_str.length() < 4) {
-                        id_num_str += temp_id_num;
+                        id_num_str += tempNum;
                     }
                     idLabel.setText("ID: " + id_num_str);
                 }
             });
         }
         deleteButton.addActionListener(new ActionListener() {
+
             public void actionPerformed(ActionEvent e) {
                 if (id_num_str.length() > 0) {
                     id_num_str = id_num_str.substring(0, id_num_str.length() - 1);
                 }
                 idLabel.setText("ID: " + id_num_str);
             }
+
         });
-        /*
-         * String[] combodata = { "Swing", "Java2D", "Java3D", "JavaMail" }; JComboBox
-         * combo = new JComboBox(combodata);
-         * 
-         * JPanel p = new JPanel(); p.add(combo);
-         * 
-         * getContentPane().add(p, BorderLayout.CENTER);
-         * 
-         * initialscreenContainer.add(panel); prevnextButton.showPrevNextButton();
-         */
+
+        return keyboardPanel;
+
+    }
+
+    private JPanel createDatePanel() {
+        JPanel datePanel = new JPanel(new FlowLayout());
+        Calendar cal = Calendar.getInstance();
+        String[] combodata_year = new String[10];
+        String[] combodata_month = new String[12];
+        String[] combodata_date = new String[31];
+        int prev_init_year = 5;
+        int init_year = cal.get(Calendar.YEAR) - prev_init_year;
+        for (int i = 0; i < 10; i++) {
+            combodata_year[i] = String.valueOf(i + init_year);
+        }
+        for (int i = 0; i < 12; i++) {
+            combodata_month[i] = String.valueOf(i + 1);
+        }
+        for (int i = 0; i < 31; i++) {
+            combodata_date[i] = String.valueOf(i + 1);
+        }
+        JComboBox<String> combo_year = new JComboBox<>(combodata_year);
+        JComboBox<String> combo_month = new JComboBox<>(combodata_month);
+        JComboBox<String> combo_date = new JComboBox<>(combodata_date);
+        Dimension dimension_combo = new Dimension(80, 30);
+
+        combo_year.setSelectedIndex(prev_init_year);
+        combo_month.setSelectedIndex(cal.get(Calendar.MONTH));
+        combo_date.setSelectedIndex(cal.get(Calendar.DATE) - 1);
+
+        combo_year.setPreferredSize(dimension_combo);
+        combo_month.setPreferredSize(dimension_combo);
+        combo_date.setPreferredSize(dimension_combo);
+
+        datePanel.add(new JLabel("Date: "));
+        datePanel.add(combo_year);
+        datePanel.add(combo_month);
+        datePanel.add(combo_date);
+        return datePanel;
     }
 
 }
