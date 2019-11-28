98be9c2f596bf15bd018066c26acea113015633e
Dongsik <dongsiku+git@gmail.com>
2019-11-28 17:45:38 +0900

    Rename: src/ElectronicProcedureDocument.java -> src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
 
-class ElectronicProcedureDocument extends JFrame {
+class Main {
 
 	public static void main(String[] args) {
 		createElectronicProcedureDocument();
